4d799f03504a1d9a8fe304a8338ed1ea8b7fde02
veroanggra <veronicaputri497@gmail.com>
2020-04-17 16:01:02 +0700

    Dice Roller App add Logs with Timber Lib

--- DiceRoller/app/src/main/java/com/veronica/idn/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/veronica/idn/diceroller/MainActivity.kt
@@ -5,12 +5,14 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Timber.i("onCreate called")
 
         val rollButton: Button = findViewById(R.id.btn_roll)
         rollButton.setOnClickListener { rollNumber() }
@@ -44,6 +46,36 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Timber.i("onStart Called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Timber.i("onResume Called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Timber.i("onPause Called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Timber.i("onStop Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Timber.i("onDestroy Called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Timber.i("onRestart Called")
     }
 
     private fun rollNumber() {
